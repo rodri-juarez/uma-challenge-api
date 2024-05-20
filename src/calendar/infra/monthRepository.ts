2d27df8553626ee95db3b6e8a3933c07f00fc5c6
Rodrigo Juarez <juarezr100@gmail.com>
2024-05-20 12:28:47 -0300

    feat: add month repository

---
+++ src/calendar/infra/monthRepository.ts
@@ -0,0 +1,4 @@
+import {BaseRepository} from '../../helpers/repositories/baseRepository';
+import {CalendarMonth} from "../domain/calendarMonth";
+
+export class MonthRepository extends BaseRepository<CalendarMonth> {}
