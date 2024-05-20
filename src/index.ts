12ba22957c7b5647dba29612d35ca56e4150d055
Rodrigo Juarez <juarezr100@gmail.com>
2024-05-20 12:27:03 -0300

    feta: add middelwares

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import 'dotenv/config';
+
+import express from 'express';
+import cors from 'cors'
+import helmet from 'helmet';
+import RateLimit from 'express-rate-limit';
+import {calendarRouter} from './routes/calendar';
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.disable('x-powered-by');
+
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 20,
+});
+
+app.use(limiter)
+
+app.use('/calendar', calendarRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
